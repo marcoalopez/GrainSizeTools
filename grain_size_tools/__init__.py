9ec57c6eeb7f471add002a617ab21f2815ba96d2
Lindsey Heagy <lindseyheagy@gmail.com>
2018-09-16 22:46:37 -0700

    update packaging to follow standard python practices

---
+++ grain_size_tools/__init__.py
@@ -0,0 +1,7 @@
+from . import piezometers
+from . import plots
+from . import tools
+from .GrainSizeTools_script import (
+    extract_column, area2diameter, calc_grain_size, Saltykov, calc_shape,
+    confidence_interval, calc_diffstress
+)
