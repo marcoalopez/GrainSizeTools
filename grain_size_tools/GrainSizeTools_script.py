bbe2e2a726d44cd330b6d8ff8b6367dd4b0cc818
Marco A. Lopez-Sanchez <marcoalopez@protonmail.com>
2024-05-29 15:28:05 +0200

    remove python files
    no longer neccesary

--- grain_size_tools/GrainSizeTools_script.py
+++ grain_size_tools/GrainSizeTools_script.py
@@ -211,10 +211,51 @@ def get_filepath():
     return file_path
 
 
+welcome = """
+======================================================================================
+Welcome to GrainSizeTools script
+======================================================================================
+A free open-source cross-platform script to visualize and characterize grain size
+population and estimate differential stress via paleopizometers.
+
+Version: 3.2.0
+Documentation: https://github.com/marcoalopez/GrainSizeTools/wiki
+
+Type function_list() to get a list of the main methods
+"""
+
+info = """
+======================================================================================
+List of main functions
+======================================================================================
+summarize              -> get the properties of the data population
+conf_interval          -> estimate a robust confidence interval using the t-distribution
+calc_diffstress        -> estimate diff. stress from grain size using piezometers
+
+plot.distribution      -> visualize the distribution of grain sizes and locate the averages
+plot.qq_plot           -> test the lognormality of the dataset (q-q plot + Shapiro-Wilk test)
+plot.area_weighted     -> visualize the area-weighed distribution of grain sizes
+plot.normalized        -> visualize a normalized distribution of grain sizes
+
+stereology.Saltykov    -> approximate the actual grain size distribution via the Saltykov method
+stereology.two_step    -> approximate the lognormal distribution to the actual distribution
+======================================================================================
+
+You can get more information about the methods using the following ways:
+    (1) Typing ? or ?? after the function name, e.g. summarize?
+    (2) Typing help plus the name of the function, e.g. help(summarize)
+    (3) In JupyterLab by enabling the "Show contextual help"
+"""
+
+def function_list():
+    print(info)
+    return None
+
+
 if __name__ == "__main__":
+    print(welcome)
     # import grain_size_tools modules
     import plot
     import averages
     import stereology
     import template
-    import get
